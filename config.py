2e4fe97d85c090aa7c468898b4ba73d412021574
alphaxleonidas <alphaxleonidas@gmail.com>
2026-03-29 20:51:39 +0500

    Refactor comments and add MAX_CONTROLLERS setting

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# set to True if you need some debug output in the console, generally not needed
+# set to True if you need some debug output in the console
 DEBUG = False
 
 # Deadzone settings, CENTER should be left untouched
@@ -6,8 +6,11 @@ CENTER = 128
 DEADZONE_L = 10  # Left stick
 DEADZONE_R = 10  # Right stick
 
-#enables or disables the shortcut (PS Button + Start) to disconnect the controller on use. set to "False" if not desired
+# enables or disables the shortcut (PS Button + Start) to disconnect the controller on use
 BT_SWITCH = True
 
-#change the name of the created virtual controller
-controllerName="Virtual Uinput Controller"
+# change the name of the created virtual controller(s)
+controllerName = "Virtual Uinput Controller"
+
+# Maximum number of controllers to support (0 for unlimited)
+MAX_CONTROLLERS = 0
